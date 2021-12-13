8aa101b5a42cd08549058af93e9139fdf530179f
CassianoCardoso <dj_kassiano@hotmail.com>
2021-12-12 22:49:32 -0300

    Cadastro de séries

---
+++ Series/Classes/SerieRepositorio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Series.interfaces;
+
+namespace Series
+{
+    class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie objeto)
+        {
+            listaSerie[id] = objeto;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void insere(Serie objeto)
+        {
+            listaSerie.Add(objeto);
+        }
+
+        public List<Serie> Listta()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
---
+++ Series/interfaces/IRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Series.interfaces
+{
+    public interface IRepositorio<T>
+    {
+        List<T> Listta();
+        T RetornaPorId(int id);
+
+        void insere(T entidade);
+
+        void Exclui(int id);
+        void Atualiza(int id, T entidade);
+        int ProximoId();
+    }
+
+}
